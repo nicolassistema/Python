20b7379dcf6d5ab26df2a356d1222752f3b43c1f
Nicolas Letticugna <nicolas.letticugna@gmail.com>
2026-03-04 16:00:46 -0300

    subiendo cambios de la clase 14

--- Udemy/Dia14/Proyecto/asistencia.py
+++ Udemy/Dia14/Proyecto/asistencia.py
@@ -2,7 +2,7 @@ import cv2
 from cv2 import *
 import face_recognition as fr
 import os
-
+import numpy as np
 
 
 #crear una base de datos
@@ -39,9 +39,51 @@ def codificar(imagenes):
 
 lista_empleados_codificada = codificar(mis_imagenes)
 
-print(len(lista_empleados_codificada))
+#tomar una imagen de camara web
+captura = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+#leer imagen de la camara
+exito, imagen = captura.read()
+
+if not exito:
+    print('No hay imagen')
+else:
+    #reconocer camara en captura
+    cara_captura = fr.face_locations(imagen)
+
+    #codifcar cara para captura
+    cara_captura_codificada = fr.face_encodings(imagen,cara_captura)
+
+    #buscar coincidencias
+    for caracodif, caraubic in zip(cara_captura_codificada,cara_captura):
+        coincidencias = fr.compare_faces(lista_empleados_codificada,caracodif)
+        distancias = fr.face_distance(lista_empleados_codificada,caracodif)
+
+        print(distancias)
+
+        indice_coincidencia = np.argmin(distancias)
+
+        #mostrar coincidencia si las hay
+        if distancias[indice_coincidencia] > 0.6:
+            print('No coincide con ninguno de nuestros empleados')
+        else:
+
+            print('Bienvenido empleado')
+            #buscar el nombre del empleado encontrado
+            nombre = nombres_empleados[indice_coincidencia]
+
+            y1, x2, y2, x1 = caraubic
+            cv2.rectangle(imagen,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(imagen,(x1,y2 - 35),(x2,y2),(0,0,255),cv2.FILLED)
+            cv2.putText(imagen, nombre, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,2555,255), 2)
+            
 
 
+            #motrar la imagen obtenida
+            cv2.imshow('Imagen web', imagen)
+
+            #mantener ventana abiera
+            cv2.waitKey(0)
 
 
 
