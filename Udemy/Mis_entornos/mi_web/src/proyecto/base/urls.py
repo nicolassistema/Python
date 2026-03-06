d957aa5cee8b2557aecfc952acef9f79801e5839
Nicolas Letticugna <nicolas.letticugna@gmail.com>
2026-03-06 15:34:59 -0300

    subiendo cambios de la clase 16

---
+++ Udemy/Mis_entornos/mi_web/src/proyecto/base/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import ListaPendientes, DetalleTarea
+
+
+urlpatterns = [path('', ListaPendientes.as_view(), name='pendientes'),
+               path('tarea/<int:pk>', DetalleTarea.as_view(), name='tarea')]
+
+
+
