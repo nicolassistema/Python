06e3d5bb43ee3e21e29cc594f1c01bb263f13a1e
Nicolas Letticugna <nicolas.letticugna@gmail.com>
2026-03-06 16:35:49 -0300

    subiendo cambios de la clase 16 - editar tarea

--- Udemy/Mis_entornos/mi_web/src/proyecto/base/urls.py
+++ Udemy/Mis_entornos/mi_web/src/proyecto/base/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ListaPendientes, DetalleTarea, CreateTarea
+from .views import ListaPendientes, DetalleTarea, CreateTarea, EditarTarea
 
 
 urlpatterns = [path('', ListaPendientes.as_view(), name='tareas'),
                path('tarea/<int:pk>', DetalleTarea.as_view(), name='tarea'),
-               path('crear-tarea/', CreateTarea.as_view(), name='crear-tarea')]
+               path('crear-tarea/', CreateTarea.as_view(), name='crear-tarea'),
+               path('editar-tarea/<int:pk>', EditarTarea.as_view(), name='editar-tarea')]
 
 
 
--- Udemy/Mis_entornos/mi_web/src/proyecto/base/views.py
+++ Udemy/Mis_entornos/mi_web/src/proyecto/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Tarea
 
@@ -20,6 +20,12 @@ class DetalleTarea(DetailView):
 class CreateTarea(CreateView):
     model = Tarea
     fields = '__all__'
+    success_url = reverse_lazy('tareas')
+
+
+class EditarTarea( UpdateView):
+    model = Tarea
+    fields = ['titulo', 'descripcion', 'completo']
     success_url = reverse_lazy('tareas')
 
 
