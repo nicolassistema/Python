b73caa7199b45d540ee66c12fc9e2beb7476e5df
Nicolas Letticugna <nicolas.letticugna@gmail.com>
2026-03-06 22:38:25 -0300

    subiendo cambios de la clase 16 - registrar

--- Udemy/Mis_entornos/mi_web/src/proyecto/base/urls.py
+++ Udemy/Mis_entornos/mi_web/src/proyecto/base/urls.py
@@ -1,13 +1,5 @@
 from django.urls import path
-from .views import (
-    ListaPendientes,
-    DetalleTarea,
-    CrearTarea,
-    EditarTarea,
-    EliminarTarea,
-    Logueo,
-    PaginaRegistro,
-)
+from .views import ListaPendientes, DetalleTarea, CrearTarea, EditarTarea, EliminarTarea, Logueo, PaginaRegistro
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
@@ -15,6 +7,7 @@ urlpatterns = [
     path('login/', Logueo.as_view(), name='login'),
     path('registro/', PaginaRegistro.as_view(), name='registro'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+
     path('tarea/<int:pk>/', DetalleTarea.as_view(), name='tarea'),
     path('crear-tarea/', CrearTarea.as_view(), name='crear-tarea'),
     path('editar-tarea/<int:pk>/', EditarTarea.as_view(), name='editar-tarea'),
